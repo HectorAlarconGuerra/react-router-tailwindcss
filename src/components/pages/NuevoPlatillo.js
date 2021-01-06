fce5dffa0bc62b1914c7c7559f26daf0146c7131
hectoralarcon <hectoralarconguerra@gmail.com>
2021-01-06 16:05:13 -0500

    formik validaciones

--- src/components/pages/NuevoPlatillo.js
+++ src/components/pages/NuevoPlatillo.js
@@ -1,7 +1,113 @@
+import React from "react";
+import { useFormik } from "formik";
+
 export default function NuevoPlatillo() {
+  //validación y leer los datos del formulario
+  const formik = useFormik({
+    initialValues: {
+      nombre: "",
+      precio: "",
+      categoria: "",
+      imagen: "",
+      descripcion: "",
+    },
+    onSubmit: (datos) => {
+      console.log(datos);
+    },
+  });
+
   return (
     <>
       <h1 className="text-3xl font-light mb-4">Nuevo Platillo</h1>
+      <div className="flex justify-center mt-10">
+        <div className="w-full max-w-3xl">
+          <form onSubmit={formik.handleSubmit}>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="nombre"
+              >
+                Nombre
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="nombre"
+                type="text"
+                placeholder="Nombre Platillo"
+                value={formik.values.nombre}
+                onChange={formik.handleChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="precio"
+              >
+                Precio
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="precio"
+                type="number"
+                placeholder="20"
+                min="0"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="nombre"
+              >
+                Categoría
+              </label>
+              <select
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="precio"
+                name="categoria"
+              >
+                <option value="">--Selecione --</option>
+                <option value="desayuno">Desayuno</option>
+                <option value="comida">Comida</option>
+                <option value="cena">Cena</option>
+                <option value="bebida">Bebidas</option>
+                <option value="postre">Postre</option>
+                <option value="ensalada">Ensaladas</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="imagen"
+              >
+                Imagen
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="imagen"
+                type="file"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="descripcion"
+              >
+                Descripción
+              </label>
+              <textarea
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-40"
+                id="descripcion"
+                placeholder="Descripción del Platillo"
+              ></textarea>
+            </div>
+            <input
+              type="submit"
+              className="bg-gray-800 hover:bg-gray-900 w-full mt-5 p-2 text-white uppercase font-bold"
+              value="Agregar Platillo"
+            />
+          </form>
+        </div>
+      </div>
     </>
   );
 }
