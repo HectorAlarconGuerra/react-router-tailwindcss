5b25bf988495400868306af84e0b1acb42d67587
HectorAlarconGuerra <33630876+HectorAlarconGuerra@users.noreply.github.com>
2021-01-12 16:12:41 -0500

    Colleccion firebase sencillo
    Crear Colleccion

--- src/components/pages/NuevoPlatillo.js
+++ src/components/pages/NuevoPlatillo.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {useContext}from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import {FirebaseContext} from '../../firebase';
+import {useNavigate} from 'react-router-dom';
 
 export default function NuevoPlatillo() {
+
+  // Context con las operaciones de firebase
+  const {firebase} = useContext(FirebaseContext);
+  
+  //Hook para redireccionar
+  const navigate = useNavigate();
+
+
   //validación y leer los datos del formulario
   const formik = useFormik({
     initialValues: {
@@ -24,8 +34,16 @@ export default function NuevoPlatillo() {
         .min(10, "La descripción debe ser más larga")
         .required("La descripción es obligatoria"),
     }),
-    onSubmit: (datos) => {
-      console.log(datos);
+    onSubmit: platillo => {
+      try{
+        platillo.existencia=true;
+        firebase.db.collection('productos').add(platillo)
+
+        //Redireccionar
+        navigate('/menu');
+      }catch(error){
+        console.log(error);
+      }
     },
   });
 
